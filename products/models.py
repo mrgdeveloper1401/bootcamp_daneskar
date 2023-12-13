cac4ab636cc0fe4d7fa94d6aa308f0caac31786e
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-12-13 23:52:06 +0330

    create model category and product

--- products/models.py
+++ products/models.py
@@ -1,4 +1,40 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey
 
 
+class Category(MPTTModel):
+    name = models.CharField(_("category name"), max_length=50)
+    parent = TreeForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='children')    
+    slug = models.SlugField()
+    is_public = models.BooleanField(default=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='category_images/%Y/%M/%d')
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Category_detail", kwargs={"pk": self.pk})
+
+
+class Product(models.Model):
+    fa_name = models.CharField(_("persion name"), max_length=150)
+    en_name = models.CharField(_("english name"), max_length=150)
+    slug = models.SlugField(max_length=150)
+    description = models.TextField()
+    product = models.ForeignKey(Category, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return self.en_name
+    
+    
+    class Meta:
+        verbose_name = _("product")
+        verbose_name_plural = _("products")
+    
+    
