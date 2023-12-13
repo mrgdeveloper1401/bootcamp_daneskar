4b345f85a34b19d5500c9f8a213f261b980f693b
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-12-13 21:29:16 +0330

    dupilcated setting

--- manage.py
+++ manage.py
@@ -2,11 +2,17 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from digikala.settings import base
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digikala.settings')
+    
+    if base.DEBUG:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digikala.settings.base')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digikala.settings.production')
+        
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
